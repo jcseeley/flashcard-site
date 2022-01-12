60ae4fcde09e11dbcc2e65578b3b835f163fad09
Jase Seeley <jcseeley31590@yahoo.com>
2022-01-11 20:45:24 -0800

    add all flashcard buttons.

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+  $("button#js").click(function() {
+  alert("JavaScript- an object-oriented computer programming language commonly used to create interactive effects within web browsers.");
+  });
+
+  $("button#op").click(function() {
+    alert("Operators- a special character (or characters) that indicates an action to be performed.");
+  });
+
+  $("button#var").click(function() {
+    alert("Variable- a container that stores some information such as a number, an object, or a string.");
+  });
+
+  $("button#varnc").click(function() {
+    alert("Variable naming conventions- variable names should be self descriptive and written in lower camel case.");
+  });
+
+  $("button#func").click(function() {
+    alert("Function- a bundle of code that performs a set of procedures.");
+  });
+
+  $("button#meth").click(function() {
+    alert("Methods- a kind of function that is called on something.");
+  });
+
+  $("button#arg").click(function() {
+    alert("Argument- the value that is passed into a parameter.");
+  });
+
+  $("button#par").click(function() {
+    alert("Parameter- a placeholder for an argument.");
+  });
+
+  $("button#ret").click(function() {
+    alert("Return- ends function execution and specifies a value to be returned to the function caller.");
+  });
+
+  $("button#chain").click(function() {
+    alert("Chaining Methods- a mechanism of calling a method on another method of the same object.");
+  });
+
+  $("button#strings").click(function() {
+    alert("String- a sequence of one or more characters that may consist of letters, numbers, or symbols.");
+  });
+
+  $("button#boo").click(function() {
+    alert("Boolean- a boolean can be one of two values: true or false.");
+  });
+
+  $("button#undef").click(function() {
+    alert("Undefined- means something hasn't been defined yet.");
+  });
+
+  $("button#nan").click(function() {
+    alert("NaN- not a number.");
+  });
+
+  $("button#null").click(function() {
+    alert("Null- means nothing. We will sometimes want to assign a null value to a thing because we don't know what value it should have yet.");
+  });
+
+  $("button#alert").click(function() {
+    alert("Alert- displays an alert dialog box that consists of some specified message and an OK button.");
+  });
+
+  $("button#com").click(function() {
+    alert("Comments- used to add information about the code, warnings or suggestions so that end user can easily interpret the code.");
+  });
+
+  $("button#jq").click(function() {
+    alert("jQuery- a JavaScript library that makes it easier to incorporate JavaScript on your web pages.");
+  });
+
+  $("button#att").click(function() {
+    alert("JavaScript- defintion.");
+  }); 
+});
